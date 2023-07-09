1bba7a4b5790a41375268ca37f9cdd275322f7ec
James Newman <jdnewman85@gmail.com>
2023-07-08 19:28:01 -0600

    Use new camera viewport_to_world_2d function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use bevy::prelude::*;
-
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::window::{PresentMode, WindowMode, PrimaryWindow};
-
-mod helpers;
+use bevy::window::PrimaryWindow;
 
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod helpers;
 
 #[derive(Component)]
 struct MainCamera;
@@ -24,45 +22,36 @@ fn clickable_sprites(
     // TODO Assumes single camera marked as MainCamera
     let (camera, camera_transform) = camera_q.single();
 
-    let wnd = window_q.get_single().unwrap();
+    let window = window_q.single();
+    let Some(screen_pos) = window.cursor_position() else { return; };
+    let Some(world_pos) = camera.viewport_to_world_2d(camera_transform, screen_pos) else { return; };
 
-    if let Some(screen_pos) = wnd.cursor_position() {
-        let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
-        let screen_ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
-        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
-        let world_pos = ndc_to_world.project_point3(screen_ndc.extend(-1.0));
-        let world_pos: Vec2 = world_pos.truncate();
-
-        //TEMP Let's hack in some mouse collision checks
-        for (transform, mut sprite, mut texture_handle) in clickable_sprite_query.iter_mut() {
-            let Some(image) = images.get(&texture_handle.clone()) else {
-                dbg!("BUG? Image err");
-                continue;
-            };
-            let delta = world_pos - transform.translation().truncate();
-            let delta_minus_image = delta.abs() - (image.size()/2.0);
-            if delta_minus_image.max_element() <= 0.0 {//if delta_minus_image.is_negative_bitmask() == 0b11 {
-                //dbg!(world_pos.x, world_pos.y);
-                sprite.color = Color::rgba(1.0, 0.0, 0.0, 1.0);
-                *texture_handle = asset_server.load("white_square_32.png");
-            } else {
-                sprite.color = Color::WHITE;
-            }
+    //TEMP Let's hack in some mouse collision checks
+    for (transform, mut sprite, mut texture_handle) in clickable_sprite_query.iter_mut() {
+        let Some(image) = images.get(&texture_handle.clone()) else { dbg!("BUG? Image err"); continue; };
+        let delta = world_pos - transform.translation().truncate();
+        let delta_minus_image = delta.abs() - (image.size()/2.0);
+        if delta_minus_image.max_element() <= 0.0 {
+            //dbg!(world_pos.x, world_pos.y);
+            sprite.color = Color::rgba(1.0, 0.0, 0.0, 1.0);
+            *texture_handle = asset_server.load("white_square_32.png");
+        } else {
+            sprite.color = Color::WHITE;
         }
+    }
 
-        // Atlas
-        for (transform, mut sprite, texture_handle) in clickable_atlas_sprite_query.iter_mut() {
-            let texture_atlas = texture_atlases.get(texture_handle).unwrap();
-            let image = texture_atlas.textures[sprite.index];
-            let delta = world_pos - transform.translation().truncate();
-            let delta_minus_image = delta.abs() - (image.size()/2.0);
-            if delta_minus_image.max_element() <= 0.0 {//if delta_minus_image.is_negative_bitmask() == 0b11 {
-                //dbg!(world_pos.x, world_pos.y);
-                sprite.color = Color::rgba(1.0, 0.0, 0.0, 1.0);
-                sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
-            } else {
-                sprite.color = Color::WHITE;
-            }
+    // Atlas
+    for (transform, mut sprite, texture_handle) in clickable_atlas_sprite_query.iter_mut() {
+        let texture_atlas = texture_atlases.get(texture_handle).unwrap();
+        let image = texture_atlas.textures[sprite.index];
+        let delta = world_pos - transform.translation().truncate();
+        let delta_minus_image = delta.abs() - (image.size()/2.0);
+        if delta_minus_image.max_element() <= 0.0 {
+            //dbg!(world_pos.x, world_pos.y);
+            sprite.color = Color::rgba(1.0, 0.0, 0.0, 1.0);
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        } else {
+            sprite.color = Color::WHITE;
         }
     }
 }
